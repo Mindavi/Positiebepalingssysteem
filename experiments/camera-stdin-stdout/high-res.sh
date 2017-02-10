2f3d056a0a03875297692353ac9a3b772cf6a17c
Rick van Schijndel <rol3517@gmail.com>
2017-02-10 21:01:15 +0100

    Single window for high res mode

--- experiments/camera-stdin-stdout/high-res.sh
+++ experiments/camera-stdin-stdout/high-res.sh
@@ -1,1 +1,1 @@
-ffmpeg -i /dev/video0 -f image2pipe -b:v 10000k - 2>/dev/null | ./read-and-view-with-edit.exe -v -p | ./read-and-view-with-edit.exe -v
+ffmpeg -i /dev/video0 -f image2pipe -b:v 10000k - 2>/dev/null | ./read-and-view-with-edit.exe -v
