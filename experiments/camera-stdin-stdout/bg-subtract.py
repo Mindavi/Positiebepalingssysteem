18603df8d13cf0eff753cb713e6c52ae30179f30
Rick van Schijndel <rol3517@gmail.com>
2017-02-11 08:34:31 +0100

    Print to stdout the changed video

--- experiments/camera-stdin-stdout/bg-subtract.py
+++ experiments/camera-stdin-stdout/bg-subtract.py
@@ -2,6 +2,8 @@
 
 import cv2
 import ImageReader
+import numpy as np
+import sys
 
 escape_key = 27
 
@@ -13,9 +15,10 @@ while True:
     imgread, img = reader.decode()
     if imgread:
         fgmask = fgbg.apply(img)
-        cv2.imshow("frame", fgmask)
-    key = cv2.waitKey(1) & 0xFF
-    if key == escape_key:
-        break
+        retval, encodedimage = cv2.imencode(".jpg", fgmask)
+        np.savetxt(sys.stdout, encodedimage, fmt='%c', newline='')
+        key = cv2.waitKey(1) & 0xFF
+        if key == escape_key:
+            break
 
 cv2.destroyAllWindows()
