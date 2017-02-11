f158f0067a7454f8df0e4bdedc7f90b57f430f0c
Rick van Schijndel <rol3517@gmail.com>
2017-02-11 08:07:02 +0100

    Changed files to use new ffmpeg.sh

--- experiments/camera-stdin-stdout/multiple.sh
+++ experiments/camera-stdin-stdout/multiple.sh
@@ -1,1 +1,1 @@
-ffmpeg -i /dev/video0 -f image2pipe - 2>/dev/null | ./read-and-view-video.exe --print --verbose | ./read-and-view-video.exe --verbose
+./ffmpeg.sh | ./read-and-view-video.exe --print --verbose | ./read-and-view-video.exe --verbose
