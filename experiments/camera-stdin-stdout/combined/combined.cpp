a6b859efd67345c9a2175c760c2f99c63c5e5cb8
Rick van Schijndel <rol3517@gmail.com>
2018-09-11 00:36:06 +0200

    remove unused code, put vars where they belong

--- experiments/camera-stdin-stdout/combined/combined.cpp
+++ experiments/camera-stdin-stdout/combined/combined.cpp
@@ -2,16 +2,9 @@
 #include <opencv2/videoio.hpp>  // for videocapture
 #include <opencv2/video.hpp>  // for backgroundsubtractor
 #include <opencv2/imgproc.hpp>  // for blur, dilation, erosion
-#include <opencv2/features2d.hpp>  // for simpleblobdetector
 
 #include <iostream>
 
-const int bg_history = 500;
-const int threshold = 16;  // default value
-const bool detectShadows = false;  // non-default
-const int minimal_area = 200;
-const int maximal_area = 500;
-const auto learning_rate_divider = 10000;
 
 void show(std::string title, const cv::Mat& image) {
     cv::imshow(title.c_str(), image);
@@ -19,15 +12,17 @@ void show(std::string title, const cv::Mat& image) {
     if (c == 27) exit(0);
 }
 
+const auto learning_rate_divider = 10000;
+
 void subtract(cv::Ptr<cv::BackgroundSubtractor> pMOG2,
     cv::Mat* image,
     cv::Mat* background,
     int learning_rate) {
-    auto learning_rate_float = learning_rate / static_cast<double>(learning_rate_divider);
+    auto rate = learning_rate / static_cast<double>(learning_rate_divider);
     if (learning_rate <= 0) {
-        learning_rate_float = -1.0;
+        rate = -1.0;
     }
-    pMOG2->apply(*image, *background, learning_rate_float);
+    pMOG2->apply(*image, *background, rate);
 }
 
 void blur(cv::Mat* input_output, int blur_size) {
@@ -57,26 +52,6 @@ void dilate(cv::Mat* input_output, int dilation_size) {
         cv::Size(2*dilation_size, 2*dilation_size),
         cv::Point(dilation_size, dilation_size));
     cv::dilate(*input_output, *input_output, element);
-}
-
-cv::Ptr<cv::SimpleBlobDetector> p_blob;
-
-void detectBlobs(const cv::Mat& image, int camera_number) {
-    std::vector<cv::KeyPoint> keypoints;
-    p_blob->detect(image, keypoints);
-
-    if (keypoints.size() <= 0) {
-        return;
-    }
-    cv::KeyPoint biggest;
-
-
-    for (const auto& keypoint : keypoints) {
-        if (keypoint.size > biggest.size) {
-            biggest = keypoint;
-        }
-    }
-    std::cout << camera_number << ":" << static_cast<int>(biggest.pt.x) << "," << static_cast<int>(biggest.pt.y) << std::endl;
 }
 
 cv::Point2f findContourMiddle(const std::vector<cv::Point>& contour) {
@@ -109,7 +84,6 @@ void findContours(const cv::Mat& image, int camera_number) {
     }
 
     cv::Mat drawing = cv::Mat::zeros(image.size(), CV_8UC3);
-    cv::Scalar color = cv::Scalar(255, 255, 255);
 
     int index = 0;
     double biggest_area = 0.0;
@@ -127,10 +101,9 @@ void findContours(const cv::Mat& image, int camera_number) {
         static_cast<int>(middle.x) << "," <<
         static_cast<int>(middle.y) << std::endl;
 
-    // cv::drawContours(drawing, contours, index, color, 2, 8, hierarchy, 0, cv::Point());
-
-    /// Show in a window
-    // show("contours", drawing);
+    cv::Scalar color = cv::Scalar(255, 255, 255);
+    cv::drawContours(drawing, contours, index, color, 2, 8, hierarchy, 0, cv::Point());
+    show("contours", drawing);
 }
 
 
@@ -142,6 +115,9 @@ int thread(int camera_number) {
     int dilation_size = 10;
     int learning_rate = 0;
 
+    const int bg_history = 500;
+    const int threshold = 16;  // default value
+    const bool detectShadows = false;  // non-default
 
     pMOG2 = cv::createBackgroundSubtractorMOG2(bg_history,
         threshold,
@@ -155,29 +131,17 @@ int thread(int camera_number) {
     cv::createTrackbar("dilation", control_window_name, &dilation_size, 20);
     cv::createTrackbar("learning rate", control_window_name, &learning_rate, learning_rate_divider);
 
-    cv::SimpleBlobDetector::Params params;
-    params.filterByArea = true;
-    params.filterByCircularity = false;
-    params.filterByColor = true;
-    params.blobColor = 255;
-    params.filterByConvexity = false;
-    params.minArea = minimal_area;
-    params.maxArea = maximal_area;
-    p_blob = cv::SimpleBlobDetector::create(params);
-
     std::cerr << camera_number << " started\n";
     while (1) {
         cv::Mat image;
         if (!capture.read(image) || image.empty()) {
             continue;
         }
-        // show("start", image);
         blur(&image, blur_size);
         erode(&image, erosion_size);
         dilate(&image, dilation_size);
         subtract(pMOG2, &image, &background, learning_rate);
         findContours(background, camera_number);
-        // detectBlobs(background, video_capture);
         show("transformed" + std::to_string(camera_number), background);
     }
 }
